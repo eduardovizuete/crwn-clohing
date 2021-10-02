8ae113431d43e1c0acd6e0886c9bd24ec3d5a0f2
edu <gilberto.vizuete@modinter.com.ec>
2021-10-01 22:00:05 -0500

    Google Sign In Authentication 3, state current user

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from './components/header/header-component';
+import Header from './components/header/header.component';
 import { auth } from './firebase/firebase-utils';
 
 class App extends React.Component {
-
   constructor() {
     super();
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
   unsubscribeFromAuth = null;
@@ -36,7 +35,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -45,7 +44,6 @@ class App extends React.Component {
       </div>
     );
   }
-
 }
 
 export default App;
