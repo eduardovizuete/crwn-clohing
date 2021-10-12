a0efe3cc3103514e43e7d81c8c10751644054eae
edu <gilberto.vizuete@modinter.com.ec>
2021-10-12 17:51:21 -0500

    Storing User Data In Firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,33 @@ const config = {
     measurementId: "G-1K4DF99WXL"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    // get DocumentReference
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    // get DocumentSnaphot - contains the data
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName, 
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
