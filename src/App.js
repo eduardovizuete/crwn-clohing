b2f2449a3ebdc14fa0b46ed752324df81f1f74e5
edu <gilberto.vizuete@modinter.com.ec>
2021-10-13 19:04:43 -0500

    Sign Up with email and password

--- src/App.js
+++ src/App.js
@@ -32,8 +32,11 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
+
+          console.log(this.state);
         });
       }
+
       this.setState({ currentUser:userAuth });
     });
   }
