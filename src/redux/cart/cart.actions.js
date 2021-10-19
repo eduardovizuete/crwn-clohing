6e028aa5fb0d8ac969f3a3909d0794bf022a5702
edu <gilberto.vizuete@modinter.com.ec>
2021-10-19 17:17:07 -0500

    Remove items from cart

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,8 @@ export const addItem = item => ({
     type: CardActionTypes.ADD_ITEM,
     payload: item
 });
+
+export const clearItemFromCart = item => ({
+    type: CardActionTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+});
