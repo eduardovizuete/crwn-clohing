737792977d4f2c9f9fd203b124b7fc3acc0fc8bb
edu <gilberto.vizuete@modinter.com.ec>
2021-10-18 20:19:53 -0500

    Reselect library updating application with reselect library, replaced cart selector with memoized selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,17 @@
+import { createSelector } from 'reselect';
+// input selector
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+    [selectCart],
+    cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => 
+        cartItems.reduce(
+            (accummulatedQuantity, cartItem) => 
+                accummulatedQuantity + cartItem.quantity, 0
+        )
+);
