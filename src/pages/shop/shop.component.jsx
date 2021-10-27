c18db746125ed84e25c6f0fc813bfe6567572e8e
edu <gilberto.vizuete@modinter.com.ec>
2021-10-26 19:41:31 -0500

    Section 17: HOC Patterns - With spinner HOC 2

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -9,9 +9,18 @@ import {
 
 import { updateCollections } from '../../redux/shop/shop.actions';
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
+
+const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -21,15 +30,29 @@ class ShopPage extends React.Component {
     this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({ loading: false });
     });
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
+        <Route 
+          exact 
+          path={`${match.path}`} 
+          render={(props) => (
+            <CollectionOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+        <Route 
+          exact 
+          path={`${match.path}/:collectionId`} 
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={loading} {...props}/>
+          )}
+        />
       </div>
     );
   }
