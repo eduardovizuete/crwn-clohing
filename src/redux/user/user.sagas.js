31e47bfb0c0d1fd9894e6a206e2672958f74c0ed
edu <gilberto.vizuete@modinter.com.ec>
2021-11-01 12:13:39 -0500

    Section 20: Redux-Saga - Sign out with sagas

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -4,7 +4,9 @@ import UserActionTypes from './user.types';
 
 import { 
   signInSuccess, 
-  signInFailure
+  signInFailure,
+  signOutSuccess,
+  signOutFailure
 } from './user.actions';
 
 import { 
@@ -54,22 +56,36 @@ export function* isUserAuthenticated() {
   }
 }
 
+export function* signOut() {
+  try {
+    yield auth.signOut();
+    yield put(signOutSuccess());
+  } catch(error) {
+    yield put(signOutFailure(error))
+  }
+}
+
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 };
 
 export function* onEmailSignInStart() {
-  yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_STAR, signInWithEmail);
+  yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail);
 }
 
 export function* onCheckUserSession() {
   yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated)
 }
 
+export function* onSignOutStart() {
+  yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
+}
+
 export function* userSagas() {
   yield all([
     call(onGoogleSignInStart), 
     call(onEmailSignInStart),
-    call(onCheckUserSession)
+    call(onCheckUserSession),
+    call(onSignOutStart)
   ]);
 }
