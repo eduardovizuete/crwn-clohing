7bf8b38ec04036cedb46cd4a3f5b8080010990a7
edu <gilberto.vizuete@modinter.com.ec>
2021-11-06 11:47:43 -0500

    Section 23: Payment Part 2 - Backend - separating app into client folder, add express server to help handle backend stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = '12345abcd';
+  const publishableKey = 'pk_test_51JnZDXJxskvmjyFSldK1Tjgy9LSgxX6d0o26wMOWdU1zrjRq5qVxlv5lCgHuADO91k8VgVrX1Etqn31sQGtbYKAT00MVIhDLGA';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful');
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error));
+      alert(
+        'There was an issue with your payment. Please sure you use the provided credit cart.'
+      );
+    });
   };
 
   return (
